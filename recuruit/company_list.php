d86284af67a490a99d18d2b36d85e20b57d704d3
2019stu02 <2019stu02@compB07.nagoya-vti.ac.jp>
2021-01-19 16:15:54 +0900

    1/19 isomura1 就職活動検索

--- recuruit/company_list.php
+++ recuruit/company_list.php
@@ -27,24 +27,18 @@ else{
 </head>
 <body>
 
-
-	企業名検索<br>
+	キーワード<br>
 	<form action="company_list.php" method="get" style="display: inline">
-	<input type="text" name="yourname" required="required">
-	<input type="submit" value="表示"><br><br>
+	<input type="text" name="yourname">を含む<br><br>
+	<input type="text" name="yourname2">を含まない<br><br>
+	<input type="submit" value="検索"><br><br>
 	</form>
 
 	<form action="company_list.php" method="get" style="display: inline">
 	</form>
 
-	含まない検索<br>
 	<form action="company_list.php" method="get" style="display: inline">
-	<input type="text" name="yourname2" required="required">
-	<input type="submit" value="表示"><br><br>
-	</form>
-
-	<form action="company_list.php" method="get" style="display: inline">
-	<input type="submit" value="全て表示"><br>
+	<input type="submit" value="全体表示"><br><br>
 	</form>
 
 </body>
@@ -70,7 +64,7 @@ try {
 
 //テーブル指定
 //SHOW TABLE　でテーブル名を取得してそのテーブル名でセレクト文を作ってテーブルデータを取得する
-/*****************************************検索************************************************/
+/*****************************************検索してない時************************************************/
 $sql = 'SHOW TABLES';
 $stmt = $dbh->query($sql);
 
@@ -94,7 +88,7 @@ foreach ($table_names as $key => $table_name) {
     }
 
 }
-/******************************************NOT検索**********************************************/
+/******************************************検索かけた時に使われる**********************************************/
 $sql3 = 'SHOW TABLES';
 $stmt3 = $dbh->query($sql3);
 
@@ -106,7 +100,7 @@ while ($result3 = $stmt->fetch(PDO::FETCH_NUM)){
 $table_datas2 = array();
 
 foreach ($table_names as $key => $table_name2) {
-    $sql4 = "SELECT COMPANY,PURPOSE1,PURPOSE2,PURPOSE3,CONTENTS FROM $table_name2 ";
+    $sql4 = "SELECT COMPANY,ADDRESS,PURPOSE1,PURPOSE2,PURPOSE3,CONTENTS FROM $table_name2 ";    //会社、住所、目的、実施内容
     /* ---- 変更箇所 ---- */
 
     if($table_name2 == 'report'){
@@ -119,7 +113,7 @@ foreach ($table_names as $key => $table_name2) {
 
 }
 /***********************************************************************************************/
-/**************************************検索***************************************************/
+/**************************************全表示***************************************************/
 foreach ($table_datas as $table_name => $table_data) {
 
     if (empty($table_data)) {
@@ -146,19 +140,6 @@ foreach ($table_datas as $table_name => $table_data) {
             </html>
             <?php
             }
-
-            elseif(!isset($_GET['yourname2'])  || $_GET['yourname2'] === "" ){                                                                //検索したとき
-                $yourname = $_GET['yourname'];
-                 if (strpos($val, $yourname) !== false) { ?>
-
-
-			<td><a href = "../recuruit/company_search.php?id=<?php echo $val;?>">・<?=htmlspecialchars($val,ENT_QUOTES,'UTF-8')?></a></td>
-            </html>
-
-
-			<?php
-                }
-            }
             }
                 echo "</tr>";
 
@@ -166,8 +147,9 @@ foreach ($table_datas as $table_name => $table_data) {
     }
     echo "</table>";
 }
+
 /************************************************************************************************/
-/***************************************NOT検索**************************************************/
+/***************************************検索**************************************************/
 
 foreach ($table_datas2 as $table_name2 => $table_data2) {
 
@@ -177,6 +159,7 @@ if (empty($table_data2)) {
 
 echo "<table border=0 style=border-collapse:collapse;>";
 $companys = '';
+$not_companys = '';
     foreach ($table_data2 as $record_num2 => $record_data2) {
         // レコード内のカラム数分ループ
         $company_flag = 1;
@@ -189,28 +172,59 @@ $companys = '';
                 $company_flag = 0;
             }
 
-            if(!(!isset($_GET['yourname2'])  || $_GET['yourname2'] === "" )){           //検索したとき
+            if(!(!isset($_GET['yourname'])  || $_GET['yourname'] === "" )){              //検索したとき
+                $yourname = $_GET['yourname'];
+                if (strpos($val2, $yourname) !== false) {       //含まれる場合
+                    $companys = $companys.$company;            //companysにはyournameに含まれる企業名が入る
+                  }
+            }
+
+            if(!(!isset($_GET['yourname2'])  || $_GET['yourname2'] === "" )){           //NOT検索したとき
                 $yourname2 = $_GET['yourname2'];
-                 if (strpos($val2, $yourname2) !== false) {     //yourname2 を含まない
-                     $companys = $companys.$company;            //companysにはyourname2に含まれる企業名が入る
-                     break;
+                 if (strpos($val2, $yourname2) !== false) {     //含まれる場合
+                     $not_companys = $not_companys.$company;            //companysにはyourname2に含まれる企業名が入る
                  }
             }
         }
 
                 echo "</tr>";
-
-
     }
    // echo $companys;
+    if((!(!isset($_GET['yourname'])  ||( $_GET['yourname'] === "") ))&&(!isset($_GET['yourname2'])  ||( $_GET['yourname2'] === "") )){
+        echo "「".$yourname."」を含む検索結果<br>";
+    }
+    elseif((!(!isset($_GET['yourname2'])  ||( $_GET['yourname2'] === "") ))&&(!isset($_GET['yourname'])  ||( $_GET['yourname'] === "") )){
+        echo "「".$yourname2."」を含まない検索結果<br>";
+    }
+    elseif((!(!isset($_GET['yourname2'])  ||( $_GET['yourname2'] === "") ))&&(!(!isset($_GET['yourname'])  ||( $_GET['yourname'] === "") ))){
+        echo "「".$yourname."」を含む「".$yourname2."」を含まない検索結果<br>";
+    }
+
     foreach ($table_data as $record_num => $record_data) {
+
         foreach ($record_data as $column_name => $val) {
-            if(!(!isset($_GET['yourname2'])  ||( $_GET['yourname2'] === "") )){
-                if (strpos($companys, $val) === false) {            // companysに含まれない企業を表示
+            //含む検索の場合   if文（yournameに入っていて、yourname2に入っていない場合）
+            if((!(!isset($_GET['yourname'])  ||( $_GET['yourname'] === "") ))&&(!isset($_GET['yourname2'])  ||( $_GET['yourname2'] === "") )){
+                if (strpos($companys, $val) !== false) {            // companysに含む企業を表示
+                    ?><td><a href = "../recuruit/company_search.php?id=<?php echo $val;?>">・<?=htmlspecialchars($val,ENT_QUOTES,'UTF-8')?></a></td>
+           				 </html>
+      <?php     }
+            }
+            //含まない検索の場合 if文（yournameに入っていなくて、yourname2に入っている場合）
+            elseif((!(!isset($_GET['yourname2'])  ||( $_GET['yourname2'] === "") ))&&(!isset($_GET['yourname'])  ||( $_GET['yourname'] === "") )){
+                if (strpos($not_companys, $val) === false) {            // companysに含まれない企業を表示
                         ?><td><a href = "../recuruit/company_search.php?id=<?php echo $val;?>">・<?=htmlspecialchars($val,ENT_QUOTES,'UTF-8')?></a></td>
            				 </html>
       <?php     }
             }
+            //両方の検索の場合 if文（yourname2に入っていて、yournameにも入っている場合）
+            elseif((!(!isset($_GET['yourname2'])  ||( $_GET['yourname2'] === "") ))&&(!(!isset($_GET['yourname'])  ||( $_GET['yourname'] === "") ))){
+                if ((strpos($not_companys, $val) === false) &&(strpos($companys, $val) !== false)){
+                    ?><td><a href = "../recuruit/company_search.php?id=<?php echo $val;?>">・<?=htmlspecialchars($val,ENT_QUOTES,'UTF-8')?></a></td>
+           				 </html>
+      <?php     }
+            }
+
 
         }
         echo "</tr>";
